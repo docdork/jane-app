a1bea94d6336500544bbfa3d2e88af3a75e8a6f0
David_Holliday <dhholliday@icloud.com>
2023-07-15 15:36:23 +0100

    Add and style modal

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, Vibration, Modal } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Vibration,
+  Modal,
+  Pressable,
+} from "react-native";
 import { useState, useEffect } from "react";
 import { Audio } from "expo-av";
 
 export default function App() {
   const [sound, setSound] = useState();
+  const [modalState, setModalState] = useState(false);
 
   async function playSound() {
     const { sound } = await Audio.Sound.createAsync(
@@ -25,14 +34,29 @@ export default function App() {
   function clickHandler1() {
     Vibration.vibrate();
     playSound();
+    setModalState(true);
+  }
+
+  function closeHandler() {
+    setModalState(false);
   }
 
   return (
     <View style={styles.container}>
+      <Modal visible={modalState} animationType="slide">
+        <View style={styles.container}>
+          <Text style={styles.mainText}>Timer Modal Content</Text>
+          <Pressable onPress={closeHandler} style={styles.button}>
+            <Text style={styles.text}>Done</Text>
+          </Pressable>
+        </View>
+      </Modal>
+
       <Text style={styles.mainText}>Jane's App</Text>
       <View style={styles.footerContainer}>
-        <Button onPress={clickHandler1} title={"Let's Eat"} />
-        
+        <Pressable onPress={clickHandler1} style={styles.button}>
+          <Text style={styles.text}>Let's Eat</Text>
+        </Pressable>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -42,15 +66,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
+    backgroundColor: "#451a52",
     alignItems: "center",
     justifyContent: "center",
   },
 
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 20,
+    backgroundColor: "blue",
+    borderWidth: 1,
+    borderColor: "white",
+  },
+
   mainText: {
     color: "#fff",
-    fontSize: 30,
+    fontSize: 40,
     padding: 30,
+  },
+
+  text: {
+    color: "#fff",
+    fontSize: 20,
+    padding: 10,
   },
 
   footerContainer: {

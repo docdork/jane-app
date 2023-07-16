d20114411c2d2af82e00ade9e1213b495981286d
David_Holliday <dhholliday@icloud.com>
2023-07-16 12:05:36 +0100

    Basic timer function.  Add Ping. Stop timer on main screen.

--- App.js
+++ App.js
@@ -3,10 +3,9 @@ import {
   StyleSheet,
   Text,
   View,
-  Button,
   Vibration,
-  Modal,
   Pressable,
+  Modal,
 } from "react-native";
 import { useState, useEffect } from "react";
 import { Audio } from "expo-av";
@@ -14,7 +13,15 @@ import { Audio } from "expo-av";
 export default function App() {
   const [sound, setSound] = useState();
   const [modalState, setModalState] = useState(false);
+  const [counter, setCounter] = useState(20);
 
+  async function playPing() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/snd_fragment_retrievewav-14728.mp3")
+    );
+    setSound(sound);
+    await sound.playAsync();
+  }
   async function playSound() {
     const { sound } = await Audio.Sound.createAsync(
       require("./assets/decidemp3-14575.mp3")
@@ -31,27 +38,42 @@ export default function App() {
       : undefined;
   }, [sound]);
 
+
   function clickHandler1() {
     Vibration.vibrate();
     playSound();
     setModalState(true);
+    setCounter(20);
   }
 
   function closeHandler() {
     setModalState(false);
   }
 
+  useEffect(() => {
+    if (modalState) {
+      const timer =
+        counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+      return () => clearInterval(timer);
+    }
+  }, [counter]);
+
+  if (counter === 0) {
+    playPing();
+    Vibration.vibrate();
+    setCounter(20);
+  }
+
   return (
     <View style={styles.container}>
       <Modal visible={modalState} animationType="slide">
         <View style={styles.container}>
-          <Text style={styles.mainText}>Timer Modal Content</Text>
+          <Text style={styles.timerText}>{counter}</Text>
           <Pressable onPress={closeHandler} style={styles.button}>
             <Text style={styles.text}>Done</Text>
           </Pressable>
         </View>
       </Modal>
-
       <Text style={styles.mainText}>Jane's App</Text>
       <View style={styles.footerContainer}>
         <Pressable onPress={clickHandler1} style={styles.button}>
@@ -65,10 +87,14 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 40,
     flex: 1,
     backgroundColor: "#451a52",
     alignItems: "center",
     justifyContent: "center",
+    borderColor: "white",
+    borderWidth: 20,
+    borderRadius: 55,
   },
 
   button: {
@@ -80,6 +106,7 @@ const styles = StyleSheet.create({
     backgroundColor: "blue",
     borderWidth: 1,
     borderColor: "white",
+    marginTop: 10,
   },
 
   mainText: {
@@ -97,5 +124,10 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: "center",
+  },
+  timerText: {
+    color: "#fff",
+    fontSize: 40,
+    padding: 30,
   },
 });
